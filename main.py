e24955900295076e7463865ccb30ec578994a421
MrBr00d <nvliet@gmail.com>
2022-09-26 12:23:03 +0200

    Fixed functions
    add/remove game function
    create/remove profile (folder) function
    loading/saving
    list profile + games

--- main.py
+++ main.py
@@ -18,49 +18,29 @@ class Game:
         # Game.instances.append(self)
 
 #check is save data exists and open it
-# to do: fix the Game.instances after load. is a dict cant append
-# fix ds3 getting added after load.
 def load():
+    global game_info
     if "Game_Info.pkl" in os.listdir():
         with open('Game_Info.pkl', 'rb') as f:
-            Game.instances = pickle.load(f)    
-    global game_info
-    global load_unpacked
-    game_info = {}
-    load_unpacked = [x for x in Game.instances]
-    print(load_unpacked)
-    for i in load_unpacked:
-        game_info[i] = Game.instances[f'{i}']
-    print(game_info)
-'''
-load()
-print(Game.instances)
-x = Game.instances
-x["DS3"].savefile
-print(load_unpacked[0])
-'''
-load()
-
+            game_info = pickle.load(f)
+    list_games()
 
 
 def add_game(name, savefile):
     global game_info
     if "game_info" in globals():
-        for i in game_info.keys():
-            if i in name:
-                print("Game already exists")
-                return
-            else:
-                game_info[name] = Game(name,savefile)
-                print("Added game")
-                with open("Game_Info.pkl", 'wb') as f:
-                    pickle.dump(game_info, f)
-                list_games()
-                return
+        if name in list(game_info.keys()):
+            print("Game already exists")
+            return
+        else:
+            game_info[name] = Game(name,savefile)
+            print("Added game")
+            with open("Game_Info.pkl", 'wb') as f:
+                pickle.dump(game_info, f)
+            list_games()
+            return
     else:
         game_info = {name: Game(name,savefile)}
-        # globals()[f'{name}'] = Game(name,savefile) temp out?
-        # name = game(name, savefile)
         with open("Game_Info.pkl", 'wb') as f:
             pickle.dump(game_info, f)
         print("New game added")
@@ -70,7 +50,6 @@ def add_game(name, savefile):
 def remove_game(game):
     if game in game_list:
         game_info.pop(f'{game}')
-        Game.instances.pop(f'{game}')
         with open("Game_Info.pkl", 'wb') as f:
             pickle.dump(game_info, f)
         list_games()
@@ -103,8 +82,4 @@ def remove_profile(game, profile):
     else:
         return print("Error: no such profile exists")
 
-add_game("DS2", r"C:\Users\Nick\Documents\test\DS2\DSII.sl2")
-add_game("DS3", r"C:\Users\Nick\Documents\test\DS3\DS3.sl2")
 
-# DS2 = Game("DS2", r"D:\test\DARKSII0000.sl2")
-# DS1 = Game("DS1", r"D:\test2")
